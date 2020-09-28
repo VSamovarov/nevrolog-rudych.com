cd18addf7022293097d12fed7d27527a8b74bf0f
Heychenko Victor <v.heychenko@gmail.com>
2020-09-28 19:22:25 +0300

    Начал разделять фронт и админ

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,11 +11,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/admin/app.css') }}">
 
         <!-- Scripts -->
         {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script> --}}
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/admin/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
