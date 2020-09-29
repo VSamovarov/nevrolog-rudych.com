38ce9345be75bf4e0ec45f204cf5bfe3f62e527d
Heychenko Victor <v.heychenko@gmail.com>
2020-09-29 12:22:25 +0300

    Поправли ошибки в миграциях

--- database/migrations/2019_11_07_144701_create_table_metadata.php
+++ database/migrations/2019_11_07_144701_create_table_metadata.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableMetada extends Migration
+class CreateTableMetadata extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2020_09_07_110850_create_post_translations_table.php
+++ database/migrations/2020_09_07_110850_create_post_translations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTimetableContentsTable extends Migration
+class CreatePostTranslationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -38,6 +38,6 @@ class CreateTimetableContentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('timetable_contents');
+        Schema::dropIfExists('post_translations');
     }
 }
