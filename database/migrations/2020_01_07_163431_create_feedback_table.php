b4ba657dfc6c9849e82be4c86cc2e5bc523671b5
Heychenko Victor <v.heychenko@gmail.com>
2020-09-29 10:48:59 +0300

    Установил расширение нестедсет

---
+++ database/migrations/2020_01_07_163431_create_feedback_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('feedback_name');
+            $table->string('feedback_email')->nullable();
+            $table->string('feedback_telephone',20)->nullable();
+            $table->string('feedback_message')->nullable();
+            $table->tinyInteger('viewed')->default(0);
+            $table->json('form_data')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
