26bb0eade5cbcc14b8f84c422c88c9cd4c650f88
Heychenko Victor <v.heychenko@gmail.com>
2020-09-29 13:04:37 +0300

    Класс херперов, начинаем делать сидеры

---
+++ database/factories/FeedbackFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Entity\Feedback;
+use Faker\Generator as Faker;
+
+$factory->define(Feedback::class, function (Faker $faker) {
+    return [
+        'feedback_name' => $faker->name,
+        'feedback_email' => $faker->email,
+        'feedback_telephone' => $faker->e164PhoneNumber,
+        'feedback_message' => $faker->text(200),
+    ];
+});
