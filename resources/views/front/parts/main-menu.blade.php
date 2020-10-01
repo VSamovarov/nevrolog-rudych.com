9cc8950d8effd1585ae20dfc2b71c494749c47da
Heychenko Victor <v.heychenko@gmail.com>
2020-10-01 16:07:27 +0300

    Главное меню для большого экрана

---
+++ resources/views/front/parts/main-menu.blade.php
@@ -0,0 +1,43 @@
+<ul class="navbar">
+    <li class="navbar__item active"><a class="" href="index.html">Home</a>
+    </li>
+    <li class="navbar__item"><a class="" href="about.html">About</a>
+    </li>
+    <li class="navbar__item navbar__has-submenu "><a class="" href="services.html">Services</a>
+        <!-- RD Navbar Dropdown-->
+        <ul class="navbar__sub_navbar">
+            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="single-service.html">Single Service</a></li>
+        </ul>
+    </li>
+    <li class="navbar__item navbar__has-submenu"><a class="" href="#">Blog</a>
+        <!-- RD Navbar Dropdown-->
+        <ul class="navbar__sub_navbar">
+            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="grid-blog.html">Grid Blog</a></li>
+            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="sidebar-blog.html">Sidebar Blog</a></li>
+            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="single-blog-post.html">Single Blog Post</a>
+            </li>
+        </ul>
+    </li>
+    <li class="navbar__item navbar__has-submenu active"><a class="" href="#">Pages</a>
+
+        <ul class="navbar__sub_navbar">
+            <li class="navbar__item"><a class="" href="careers.html">Careers</a>
+            </li>
+            <li class="navbar__item"><a class="" href="single-job.html">Single
+                    Job</a></li>
+            <li class="navbar__item"><a class="" href="team.html">Team</a></li>
+            <li class="navbar__item"><a class="" href="blog-layouts.html">Blog
+                    Layouts</a></li>
+            <li class="navbar__item"><a class="" href="about-us.html">About Us</a>
+            </li>
+            <li class="navbar__item"><a class="" href="modern-gallery.html">Modern
+                    Gallery</a></li>
+            <li class="navbar__item"><a class="" href="grid-gallery.html">Grid
+                    Gallery</a></li>
+            <li class="navbar__item"><a class="" href="masonry-gallery.html">Masonry Gallery</a></li>
+        </ul>
+
+    </li>
+    <li class="navbar__item"><a class="" href="contacts.html">Contacts</a>
+    </li>
+</ul>
--- resources/views/front/parts/page-header.blade.php
+++ resources/views/front/parts/page-header.blade.php
@@ -1,29 +1,47 @@
-<div class="container page-header">
-    <div class="brand">
-        <a href="index.html">
-            <img src="{{ asset('img/logo.png') }}" alt="" width="128" height="37">
-        </a>
-    </div>
-
-    <article class="unit align-items-center">
-        <div class="unit-left"><span class="linearicons-telephone2"></span></div>
-        <div class="unit-body">
-            <ul class="list-0">
-                <li><a class="link-default-light" href="tel:#">1-800-1234-567</a></li>
-                <li><a class="link-default-light" href="tel:#">1-800-8763-765</a></li>
-            </ul>
+<div class="page-header">
+    <div class="container page-header__top">
+        <div class="brand">
+            <a href="index.html">
+                <img src="{{ asset('img/logo.png') }}" alt="" width="128" height="37">
+            </a>
         </div>
-    </article>
+        <div class="contacts-wrapper">
+            <div class="contacts">
+                <div class="contacts__icon"><span class="linearicons-telephone2"></span></div>
+                <div class="contacts__content">
+                    <p>
+                        <a class="text-nowrap" href="tel:#">1-800-1234-567</a>
+                    <p>
+                    </p>
+                    <a class="text-nowrap" href="tel:#">1-800-8763-765</a>
+                    </p>
+                </div>
+            </div>
 
-    <article class="unit align-items-center">
-        <div class="unit-left"><span class="linearicons-location"></span></div>
-        <div class="unit-body"><a class="list-inline-comma-default link-default-light text-nowrap" href="#">2130 Fulton
-                Street <br> San Diego, CA 94117-1080</a></div>
-    </article>
+            <div class="contacts">
+                <div class="contacts__icon"><span class="linearicons-location"></span></div>
+                <div class="contacts__content">
+                    <p>
+                        <a class="" href="#">2130 Fulton Street, San
+                            Diego, CA 94117-1080</a>
+                    </p>
+                </div>
+            </div>
 
-    <ul class="custom-list">
-        <li><a class="icon-facebook" href="#"></a></li>
-        <li><a class="icon-twitter" href="#"></a></li>
-        <li><a class="icon-linkedin-1" href="#"></a></li>
-    </ul>
-    <div>
+            <div class="social-links">
+                <p class="social-links__item"><a class="icon-facebook" href="#"></a></p>
+                <p class="social-links__item"><a class="icon-twitter" href="#"></a></p>
+                <p class="social-links__item"><a class="icon-linkedin-1" href="#"></a></p>
+            </div>
+        </div>
+    </div>
+    <div class="page-header__menu">
+        <div class="container main-menu-wrapper">
+            <div class="main-menu">
+
+                @include('front.parts.main-menu')
+            </div>
+            <div class="search"></div>
+        </div>
+    </div>
+</div>
