20b9b4251dc7fe05a604107a9a9da2dbf60e48b3
Heychenko Victor <v.heychenko@gmail.com>
2020-10-02 15:21:43 +0300

    Праки по стилям

--- resources/views/front/modules/main-menu.blade.php
+++ resources/views/front/modules/main-menu.blade.php
@@ -1,43 +1,43 @@
-<ul class="navbar">
-    <li class="navbar__item"><a class="" href="index.html">Home</a>
+<ul class="main-navbar">
+    <li class="main-navbar__item"><a class="" href="index.html">Home</a>
     </li>
-    <li class="navbar__item"><a class="" href="about.html">About</a>
+    <li class="main-navbar__item"><a class="" href="about.html">About</a>
     </li>
-    <li class="navbar__item navbar__has-submenu "><a class="" href="services.html">Services</a>
-        <!-- RD Navbar Dropdown-->
-        <ul class="navbar__sub_navbar">
+    <li class="main-navbar__item main-navbar__has-submenu "><a class="" href="services.html">Services</a>
+        <!-- RD main-navbar Dropdown-->
+        <ul class="main-navbar__sub_navbar">
             <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="single-service.html">Single Service</a></li>
         </ul>
     </li>
-    <li class="navbar__item navbar__has-submenu"><a class="" href="#">Blog</a>
-        <!-- RD Navbar Dropdown-->
-        <ul class="navbar__sub_navbar">
+    <li class="main-navbar__item main-navbar__has-submenu"><a class="" href="#">Blog</a>
+        <!-- RD main-navbar Dropdown-->
+        <ul class="main-navbar__sub_navbar">
             <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="grid-blog.html">Grid Blog</a></li>
             <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="sidebar-blog.html">Sidebar Blog</a></li>
             <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="single-blog-post.html">Single Blog Post</a>
             </li>
         </ul>
     </li>
-    <li class="navbar__item navbar__has-submenu"><a class="" href="#">Pages</a>
+    <li class="main-navbar__item main-navbar__has-submenu"><a class="" href="#">Pages</a>
 
-        <ul class="navbar__sub_navbar">
-            <li class="navbar__item"><a class="" href="careers.html">Careers</a>
+        <ul class="main-navbar__sub_navbar">
+            <li class="main-navbar__item"><a class="" href="careers.html">Careers</a>
             </li>
-            <li class="navbar__item"><a class="" href="single-job.html">Single
+            <li class="main-navbar__item"><a class="" href="single-job.html">Single
                     Job</a></li>
-            <li class="navbar__item"><a class="" href="team.html">Team</a></li>
-            <li class="navbar__item"><a class="" href="blog-layouts.html">Blog
+            <li class="main-navbar__item"><a class="" href="team.html">Team</a></li>
+            <li class="main-navbar__item"><a class="" href="blog-layouts.html">Blog
                     Layouts</a></li>
-            <li class="navbar__item"><a class="" href="about-us.html">About Us</a>
+            <li class="main-navbar__item"><a class="" href="about-us.html">About Us</a>
             </li>
-            <li class="navbar__item"><a class="" href="modern-gallery.html">Modern
+            <li class="main-navbar__item"><a class="" href="modern-gallery.html">Modern
                     Gallery</a></li>
-            <li class="navbar__item"><a class="" href="grid-gallery.html">Grid
+            <li class="main-navbar__item"><a class="" href="grid-gallery.html">Grid
                     Gallery</a></li>
-            <li class="navbar__item"><a class="" href="masonry-gallery.html">Masonry Gallery</a></li>
+            <li class="main-navbar__item"><a class="" href="masonry-gallery.html">Masonry Gallery</a></li>
         </ul>
 
     </li>
-    <li class="navbar__item"><a class="" href="contacts.html">Contacts</a>
+    <li class="main-navbar__item"><a class="" href="contacts.html">Contacts</a>
     </li>
 </ul>
