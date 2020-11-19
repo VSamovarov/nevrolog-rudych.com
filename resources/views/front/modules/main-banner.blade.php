98b41a91fa69339e4053c1034f7efc4f7cf15a88
Victor Heychenko <v.heychenko@gmail.com>
2020-11-19 23:30:13 +0200

    Разобрался со шрифтами

--- resources/views/front/modules/main-banner.blade.php
+++ resources/views/front/modules/main-banner.blade.php
@@ -1,2 +1,22 @@
-<div class="section main-banner bg-image" style="background-image: url({{ asset('img/breadcrumbs-image-1.jpg') }});">
+<div class="section" style="background-color: #9baacb">
+  <div class="container main-banner">
+    <div class="main-banner__wrapper" style="background-image: url({{ asset('img/Rudych/nevrolog-rudych-2.jpg') }});">
+      <div class="main-banner__content">
+        <div class="main-banner__content-inner">
+          <div class="main-banner__text font-italic text-primary h5">
+            All Kinds of Counseling
+          </div>
+          <div class="main-banner__title h1">
+            Improve<br>Your Mental<br>Well-being
+          </div>
+          <div class="main-banner__action">
+            <button>
+              Action
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </div>
