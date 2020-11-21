6d01d189b640646cfc82da3b83ffa5d79d012587
Victor Heychenko <v.heychenko@gmail.com>
2020-11-21 21:29:23 +0200

    info-blok -модуль

--- resources/views/front/modules/info-block.blade.php
+++ resources/views/front/modules/info-block.blade.php
@@ -1,5 +1,5 @@
 <style>
-:root {
+.section.info-block-section.c-{{ Illuminate\Support\Str::slug($color) }}@if(!empty($variant)).{{$variant}}@endif{
   --info-block-color: {{$color}};
 }
 </style>
@@ -12,9 +12,11 @@
         @else
         <a href="{{$item['url']}}" class="info-block">
         @endif
+          @if (!empty($item['icon']))
           <div class="info-block__icon">
             <span class="{{$item['icon']}}"></span>
           </div>
+          @endif
           <div class="info-block__text_wrapper">
             <div class="info-block__title">{{$item['title']}}</div>
             <div class="info-block__text">{{$item['text']}}</div>
--- resources/views/front/pages/main.blade.php
+++ resources/views/front/pages/main.blade.php
@@ -9,6 +9,36 @@
     $data =
     [
       [
+        'url'=> '',
+        'icon'=> 'linearicons-trophy2',
+        'title'=> '10 Years of Experience',
+        'text'=> 'Ut porttitor leo a diam sollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla at.',
+      ],
+      [
+        'url'=> '',
+        'icon'=> 'linearicons-lifebuoy',
+        'title'=> 'Free Consultations',
+        'text'=> 'Nec ultrices dui sapien eget mi. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique.',
+      ],
+      [
+        'url'=> '',
+        'icon'=> 'linearicons-clipboard-check',
+        'title'=> '100% Guaranteed',
+        'text'=> 'Integer quis auctor elit sed. Adipiscing commodo elit at imperdiet dui accumsan. Adipiscing bibendum est ultricies integer quis auctor elit.',
+      ],
+    ];
+  $color = "#984c8a";
+  $class = "info-block-section";
+  $variant = "";
+  $section_title = "";
+  @endphp
+  @include('front.modules.section',['color'=>$color,'data'=>$data, 'variant' => $variant, 'section_title'=>$section_title])
+
+
+  @php
+    $data =
+    [
+      [
         'url'=> '#',
         'icon'=> 'linearicons-user',
         'title'=> 'Individuals',
@@ -27,8 +57,40 @@
         'text'=> 'Family counseling is designed to address specific issues that affect the psychological health of the family and each of its members.',
       ],
     ];
-  $color = "brown";
-  $class = "info-block-section";
+    $color = "#503a87";
+    $class = "info-block-section";
+    $variant = "center";
+    $section_title = "Who I Work With";
   @endphp
-  @include('front.modules.section',['color'=>$color,'data'=>$data])
+  @include('front.modules.section',['color'=>$color,'data'=>$data, 'class'=>$class, 'variant' => $variant, 'section_title'=>$section_title])
+
+
+  @php
+    $data =
+    [
+      [
+        'url'=> '#',
+        'icon'=> '',
+        'title'=> 'Individuals',
+        'text'=> 'Individual counseling can help one deal with many personal topics in life such as anger, depression, anxiety, marriage and relationship challenges.',
+      ],
+      [
+        'url'=> '#',
+        'icon'=> '',
+        'title'=> 'Couples',
+        'text'=> 'My couples counseling services can not only solve the problems the couple is facing with each other, but also revive the feeling of love.',
+      ],
+      [
+        'url'=> '#',
+        'icon'=> '',
+        'title'=> 'Families',
+        'text'=> 'Family counseling is designed to address specific issues that affect the psychological health of the family and each of its members.',
+      ],
+    ];
+    $color = "#503a87";
+    $class = "info-block-section";
+    $variant = "";
+    $section_title = "";
+  @endphp
+  @include('front.modules.section',['color'=>$color,'data'=>$data, 'class'=>$class, 'variant' => $variant, 'section_title'=>$section_title])
 @endsection
