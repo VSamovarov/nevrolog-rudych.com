78d0cbe01a2d6ce79cb77dce11accbd45fe9a3b1
Victor Heychenko <v.heychenko@gmail.com>
2020-11-24 14:28:16 +0200

    Есть проблема с именами роутера

---
+++ app/Entity/Setting/Setting.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entity\Setting;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+  /**
+   * Отношения
+   */
+  public $timestamps = false;
+  public function description()
+  {
+      return $this->hasMany(Post::class);
+  }
+}
