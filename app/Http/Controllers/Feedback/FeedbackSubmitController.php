f6643c066e0cd5b5b33c58522b65dfe48e7534d4
Victor Heychenko <v.heychenko@gmail.com>
2020-12-17 19:07:33 +0200

    Убрал исключение

--- app/Http/Controllers/Feedback/FeedbackSubmitController.php
+++ app/Http/Controllers/Feedback/FeedbackSubmitController.php
@@ -8,6 +8,7 @@ use App\Entity\User\User;
 use App\Http\Controllers\Controller;
 use App\Notifications\NewFeedback;
 use App\Notifications\NewFeedbackAdmin;
+use Exception;
 use Illuminate\Support\Facades\Notification;
 use Log;
 
@@ -17,12 +18,17 @@ class FeedbackSubmitController extends Controller
   public function __invoke(FeedbackSubmitRequestForm $request, FeedbackCommands $commands)
   {
     $feedback = $commands->create($request->getDto());
-    Log::write('info', $feedback->email);
-    if(!empty($feedback->email)) $feedback->notify(new NewFeedback);
-
-    Notification::send(User::all(), new NewFeedbackAdmin($feedback));
 
     $message = "<div><h4>" . __('feedback.received_title'). "</h4>" . __('feedback.received_content') . "</div>";
+
+    try {
+      if(!empty($feedback->email)) $feedback->notify(new NewFeedback);
+      Notification::send(User::all(), new NewFeedbackAdmin($feedback));
+    }  catch (Exception $e) {
+      $message .= '<p>><small>@@@@@</small></p>';
+    }
+
+
     return response()->json($message);
   }
 }
