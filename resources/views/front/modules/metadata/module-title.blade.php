645bc2d5e4bc57905b4e04e873663e5892aaece4
Victor Heychenko <v.heychenko@gmail.com>
2020-12-31 19:06:59 +0200

    Стили для заглавий секции

--- resources/views/front/modules/metadata/module-title.blade.php
+++ resources/views/front/modules/metadata/module-title.blade.php
@@ -1,9 +1,10 @@
   @if ( !empty($data['show-section-title']) && !empty($data['section-title']) )
     <div class="container">
-      <div class="section__title h1 text-center">
+      <div class="section__title h2 text-center">
         @if (!empty($data['show-link-section-title']) && !empty($data['link-section-title']) )
         <a class="section__title-link" href="{{$data['link-section-title']}}">
           {{ App\Services\Helper::getLocalized($data['section-title']) }}
+          <span class="linearicons-arrow-right"></span>
         </a>
         @else
           {{ App\Services\Helper::getLocalized($data['section-title']) }}
